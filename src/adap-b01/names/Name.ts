dfd8d74c39dbb9a066c88fdb6c4093c3ca71b01e
Silvan Weiß <silvan.weiss@fau.de>
2025-10-15 00:21:49 +0200

    some progress but missing knowledge of masking

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -1,8 +1,9 @@
+import * as string_decoder from "node:string_decoder";
+
 export const DEFAULT_DELIMITER: string = '.';
 export const ESCAPE_CHARACTER = '\\';
 
 
-/* test */
 /**
  * A name is a sequence of string components separated by a delimiter character.
  * Special characters within the string may need masking, if they are to appear verbatim.
@@ -22,18 +23,29 @@ export class Name {
 
     /** Expects that all Name components are properly masked */
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        this.delimiter = delimiter ?? DEFAULT_DELIMITER;
+        other.forEach(component => this.components.push(component))
     }
 
     /**
-     * Returns a human-readable representation of the Name instance using user-set control characters
+     * Returns a human-readable representation of the Name instance using user-set control characters.
      * Control characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
-    }
+        return this.components.join(delimiter)
 
+        // let path: string = "";
+        // for (let i: number = 0; i < this.components.length; i++) {
+        //     if (i != 0) {
+        //         path += delimiter;
+        //     }
+        //     path += this.components[i];
+        // }
+        //
+        // return path
+    }
+// TODO: no idea what masking is, redo at later time
     /** 
      * Returns a machine-readable representation of Name instance using default control characters
      * Machine-readable means that from a data string, a Name can be parsed back in
@@ -43,28 +55,40 @@ export class Name {
         throw new Error("needs implementation or deletion");
     }
 
+    // @methodtype get-methode
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components[i] = c;
     }
 
      /** Returns number of components in Name instance */
      public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        if (i == 0) {
+            this.components.unshift(c);
+            return;
+        }
+        if (i == this.components.length - 1) {
+            this.components.push(c);
+            return;
+        }
+
+        let firstHalf: string[] = this.components.slice(0, i);
+        firstHalf.push(c);
+        this.components = firstHalf.concat(this.components.slice(i));
     }
 
     /** Expects that new Name component c is properly masked */
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
 
     public remove(i: number): void {

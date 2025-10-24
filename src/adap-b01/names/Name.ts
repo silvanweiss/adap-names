42ae9fb8af83d46cf0720d972c66a77626026153
Silvan Weiss <silvan.weiss@fau.de>
2025-10-24 23:51:53 +0200

    still find masking and escaping pretty strange, does it only use the default delimiter?

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -1,8 +1,9 @@
+import * as repl from "node:repl";
+
 export const DEFAULT_DELIMITER: string = '.';
 export const ESCAPE_CHARACTER = '\\';
 
 
-/* test */
 /**
  * A name is a sequence of string components separated by a delimiter character.
  * Special characters within the string may need masking, if they are to appear verbatim.
@@ -22,22 +23,32 @@ export class Name {
 
     /** Expects that all Name components are properly masked */
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        this.delimiter = delimiter ?? DEFAULT_DELIMITER;
+        other.forEach(component => this.components.push(component));
     }
 
     /**
-     * Returns a human-readable representation of the Name instance using user-set special characters
+     * Returns a human-readable representation of the Name instance using user-set special characters.
      * Special characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        let unmaskedComponents: string[] = [];
+
+        for (const component of this.components) {
+            let tempComponent: string = component.replaceAll("\\.", ".");
+            unmaskedComponents.push(tempComponent.replaceAll("\\\\", "\\"));
+        }
+
+        return unmaskedComponents.join(delimiter);
     }
 
+
+    // TODO: no idea what masking is, redo at later time
     /** 
-     * Returns a machine-readable representation of Name instance using default special characters
+     * Returns a machine-readable representation of Name instance using default control characters
      * Machine-readable means that from a data string, a Name can be parsed back in
-     * The special characters in the data string are the default characters
+     * The control characters in the data string are the default characters
      */
     public asDataString(): string {
         throw new Error("needs implementation or deletion");
@@ -45,31 +56,43 @@ export class Name {
 
     /** Returns properly masked component string */
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components[i] = c;
     }
 
      /** Returns number of components in Name instance */
      public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        // if (i == 0) {
+        //     this.components.unshift(c);
+        //     return;
+        // }
+        // if (i == this.components.length - 1) {
+        //     this.components.push(c);
+        //     return;
+        // }
+        //
+        // let firstHalf: string[] = this.components.slice(0, i);
+        // firstHalf.push(c);
+        // this.components = firstHalf.concat(this.components.slice(i));
+        this.components.splice(i,0, c)
     }
 
     /** Expects that new Name component c is properly masked */
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c);
     }
 
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(i,1);
     }
 
 }

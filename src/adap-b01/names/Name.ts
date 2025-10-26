8b0acf73ed4d05b91729162007f2d402aba0a6ef
Silvan Weiss <silvan.weiss@fau.de>
2025-10-26 13:07:22 +0100

    full implementation unsure about constructor and command method as well as "asDataString" vs "asString"

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -1,5 +1,3 @@
-import * as repl from "node:repl";
-
 export const DEFAULT_DELIMITER: string = '.';
 export const ESCAPE_CHARACTER = '\\';
 
@@ -22,9 +20,13 @@ export class Name {
     private components: string[] = [];
 
     /** Expects that all Name components are properly masked */
+    // @methodtype initialization-method
     constructor(other: string[], delimiter?: string) {
         this.delimiter = delimiter ?? DEFAULT_DELIMITER;
-        other.forEach(component => this.components.push(component));
+
+        other.forEach(component =>
+            this.components.push(component)
+        );
     }
 
     /**
@@ -32,44 +34,58 @@ export class Name {
      * Special characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      */
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
         let unmaskedComponents: string[] = [];
 
-        for (const component of this.components) {
-            let tempComponent: string = component.replaceAll("\\.", ".");
-            unmaskedComponents.push(tempComponent.replaceAll("\\\\", "\\"));
-        }
+        this.components.forEach(component =>
+            unmaskedComponents.push(this.asUnmaskComponent(component, delimiter))
+        );
 
         return unmaskedComponents.join(delimiter);
     }
 
+    // @methodtype conversion-method
+    private asUnmaskComponent(component: string, delimiter: string) : string {
+        const maskedDelimiter: string = ESCAPE_CHARACTER + delimiter;
+        const maskedEscapeCharacter: string = ESCAPE_CHARACTER + ESCAPE_CHARACTER;
 
-    // TODO: no idea what masking is, redo at later time
+        return component
+            .replaceAll(maskedDelimiter, delimiter)
+            .replaceAll(maskedEscapeCharacter, ESCAPE_CHARACTER);
+    }
+
     /** 
      * Returns a machine-readable representation of Name instance using default control characters
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The control characters in the data string are the default characters
      */
+    // @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(DEFAULT_DELIMITER);
     }
 
     /** Returns properly masked component string */
+    // @methodtype get-method
     public getComponent(i: number): string {
+        //const component = this.components.at(i);
         return this.components[i];
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype set-method
     public setComponent(i: number, c: string): void {
         this.components[i] = c;
     }
 
      /** Returns number of components in Name instance */
+     // @methodtype get-method
      public getNoComponents(): number {
         return this.components.length;
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public insert(i: number, c: string): void {
         // if (i == 0) {
         //     this.components.unshift(c);
@@ -83,16 +99,17 @@ export class Name {
         // let firstHalf: string[] = this.components.slice(0, i);
         // firstHalf.push(c);
         // this.components = firstHalf.concat(this.components.slice(i));
-        this.components.splice(i,0, c)
+        this.components.splice(i,0, c);
     }
 
     /** Expects that new Name component c is properly masked */
+    // @methodtype command-method
     public append(c: string): void {
         this.components.push(c);
     }
 
+    // @methodtype command-method
     public remove(i: number): void {
         this.components.splice(i,1);
     }
-
 }
